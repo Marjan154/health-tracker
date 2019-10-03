39f14a955b08896c8c2b110da520702c99b8f1ee
Xiangmin Mo <xiangminmo6121@gmail.com>
2019-10-03 03:34:52 -0400

    Created the login/reg page; very simple validation

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Route, BrowserRouter as Router } from 'react-router-dom'
+import './index.css'
+import App from './Components/App'
+import Home from './Components/Home.js'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+  <Router>
+    <div>
+      <Route exact path="/" component={App} />
+      <Route path="/home" component={Home} />
+    </div>
+  </Router>
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(routing, document.getElementById('root'))
