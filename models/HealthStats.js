1485d2ab1c0dbe7434ba486e6b87ced7cd2917a7
MarjanGit <mansar16@huntersoe.org>
2019-11-09 15:09:31 -0500

    invalid datatype

--- models/HealthStats.js
+++ models/HealthStats.js
@@ -2,38 +2,38 @@ const db = require("./db");
 const Sequelize = require("sequelize");
 
 const HealthStats = db.define("healthstats", {
-    userid: {
-        type: Sequelize.TEXT,
-        allowNull: false,
-        validate: {
-            notEmpty: true
-        }
-    },
-    healthstatid: {
-        type: Sequelize.TEXT,
-        allowNull: false,
-        validate: {
-            notEmpty: true
-        }
-    },
-    water: {
-        type: Sequelize.FLOAT
-    },
-    weight: {
-        type: Sequelize.FLOAT
-    },
-    height: {
-        type: Sequelize.FLOAT
-    },
-    sleep: {
-        type: Sequelize.FLOAT
-    },
-    steps: {
-        type: Sequelize.INT
-    },
-    calories: {
-        type: Sequelize.FLOAT
+  userid: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true
     }
-})
+  },
+  healthstatid: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  water: {
+    type: Sequelize.FLOAT
+  },
+  weight: {
+    type: Sequelize.FLOAT
+  },
+  height: {
+    type: Sequelize.FLOAT
+  },
+  sleep: {
+    type: Sequelize.FLOAT
+  },
+  steps: {
+    type: Sequelize.INTEGER
+  },
+  calories: {
+    type: Sequelize.FLOAT
+  }
+});
 
 module.exports = HealthStats;
