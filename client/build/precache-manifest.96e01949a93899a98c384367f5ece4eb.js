8bc6400f42b74567cd3d29d59e9314a900862824
MarjanGit <mansar16@huntersoe.org>
2019-11-09 16:29:29 -0500

    changed port created build folder

---
+++ client/build/precache-manifest.96e01949a93899a98c384367f5ece4eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f86eb233ada087f2e4b4f5d84153706",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c99fc0e20b1c4bd8f0d",
+    "url": "/static/css/main.e7378694.chunk.css"
+  },
+  {
+    "revision": "05e2e0fe8833c7380971",
+    "url": "/static/js/2.0c191a22.chunk.js"
+  },
+  {
+    "revision": "0c99fc0e20b1c4bd8f0d",
+    "url": "/static/js/main.a7ca90f9.chunk.js"
+  },
+  {
+    "revision": "eefbc53ae96f05c8f15f",
+    "url": "/static/js/runtime-main.709c4d38.js"
+  },
+  {
+    "revision": "698787430d6c5fceb95c74b57ce8024d",
+    "url": "/static/media/splashbg.69878743.PNG"
+  }
+]);
