c1791332bf0dfe42360cb21e41c7b95b5a6b9e9d
MarjanGit <mansar16@huntersoe.org>
2019-11-09 18:44:32 -0500

    users now create

--- models/db.js
+++ models/db.js
@@ -1,20 +1,19 @@
 const Sequelize = require("sequelize");
 
-module.exports =
-    new Sequelize(
-        process.env.PGDATABASE,
-        process.env.PGUSER,
-        process.env.PGPASSWORD,
-        {
-            host: "localhost",
-            dialect: "postgres",
-            operatorsAliases: false,
+module.exports = new Sequelize(
+  "healthtracker",
+  process.env.PGUSER,
+  process.env.PGPASSWORD,
+  {
+    host: "localhost",
+    dialect: "postgres",
+    operatorsAliases: false,
 
-            pool: {
-                max: 5,
-                min: 0,
-                acquire: 30000,
-                idle: 10000
-            }
-        }
-    );
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  }
+);
