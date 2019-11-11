efe2dabb176285292a15b74cfa04bc8bb207bd51
Xiangmin Mo <xiangminmo6121@gmail.com>
2019-11-10 19:51:12 -0500

    restructuring, bootstrap, home component

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,35 +1,47 @@
 import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
-import "../Styling/Login.css";
 import axios from "axios";
 
 class Reg extends Component {
   constructor() {
     super();
     this.state = {
-      user: {
-        username: "",
-        password: ""
-      },
+      email: '',
+      password: '',
+      password2: '',
+      hidden: true,
       redirect: false
     };
+
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onChangePass = this.onChangePass.bind(this);
+    this.onChangePass2 = this.onChangePass2.bind(this);
+    this.toggleShow= this.toggleShow.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
-  handleChange = event => {
-    const updatedUser = { ...this.state.user };
-    const inputField = event.target.name;
-    const inputValue = event.target.value;
-    updatedUser[inputField] = inputValue;
-    this.setState({ user: updatedUser });
+  onChangeEmail= (event)=> {
+    this.setState({email: event.target.value})
   };
 
-  handleClick = event => {
-    event.preventDefault();
+  onChangePass= (event)=> {
+    this.setState({password: event.target.value});
+  }
 
-    //Needs to be better
-    if (this.state.user.username !== "" && this.state.user.password !== "") {
-      const data = this.state.user;
-      data.email = "email";
+  onChangePass2= (event)=> {
+    this.setState({password2: event.target.value});
+  }
+
+  toggleShow(){
+    this.setState({hidden: !this.state.hidden});
+  }
+
+  onSubmit= (event)=> {
+    event.preventDefault();
+    if (this.state.password !== this.state.password2) {
+      alert("Passwords do not match");
+    } else {
+      const data = this.state.email;
 
       console.log(data);
       let url = "http://localhost:5000/api/users/create";
@@ -44,8 +56,6 @@ class Reg extends Component {
         .catch(error => {
           console.log(error);
         });
-    } else {
-      alert("Error");
     }
   };
 
@@ -55,29 +65,28 @@ class Reg extends Component {
     }
 
     return (
-      <div className="entire-box">
-        <p className="title">Register</p>
-        <form className="formL">
-          <label for="un">Username:</label>
-          <input
-            id="un"
-            name="username"
-            type="text"
-            value={this.state.user.username}
-            onChange={this.handleChange}
-          />
-          <label for="pw">Password:</label>
-          <input
-            id="pw"
-            name="password"
-            type="password"
-            value={this.state.user.password}
-            onChange={this.handleChange}
-          />
-          <br />
-          <button id="reg" onClick={this.handleClick}>
-            Register
-          </button>
+      <div className="container login-container" style= {{marginTop: "5%", marginBottom: "5%"}}>
+        <form onSubmit={this.onSubmit}>
+          <h3 style={{textAlign: "center", marginBottom: "5%"}}>Register</h3>
+          <div className= "form-group">  
+            <label style={{fontWeight: "bold"}} for= "email1">Email:</label>
+            <input className="form-control" type= "email" required id= "email1" pattern="[a-zA-Z0-9.-_]{1,}@[a-zA-Z0-9.-]{2,}[.]{1}[a-zA-Z]{2,}" value= {this.state.email} onChange= {this.onChangeEmail} />
+          </div>
+          <div className= "form-group">
+            <label style={{fontWeight: "bold"}} for= "pw">Password:</label>
+            <input className="form-control" type= {this.state.hidden ? "password": "text"} required id= "pw" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" value= {this.state.password} onChange= {this.onChangePass} />
+            <small className="form-text text-white">Password has to contain a <b>lowercase</b> letter, <b>uppercase</b> letter, a <b>number</b>, and it must be a <b>minimum of 8 characters</b></small>
+          </div>
+          <div className= "form-group">
+            <label style={{fontWeight: "bold"}} for= "pw2">Re-enter Password:</label>
+            <input className="form-control" type= {this.state.hidden ? "password": "text"} required id= "pw2" value= {this.state.password2} onChange= {this.onChangePass2} />
+          </div>
+          <div className= "form-group">
+            <input className="btn btn-secondary btn-sm" type="button" onClick= {this.toggleShow} value= "Show/Hide Password"/>
+          </div>
+          <div className= "form-group text-center">
+            <input className="btn btn-primary btn-lg" type="submit" value= "Register" />
+          </div>
         </form>
       </div>
     );
