3873ea55da43555bc55a5fe2bf50f58ede76b2a0
MarjanGit <mansar16@huntersoe.org>
2019-12-08 23:41:51 -0500

    nav

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -2,25 +2,28 @@ import React, { Component } from "react";
 import { Link, withRouter } from "react-router-dom";
 import styles from "../Styling/navbar.css";
 
-function Nav() {
-  return (
-    <div id="nav-container">
-      <nav className="nav">
-        <ul>
-          <li>
-            <Link to="/Home">Home</Link>
-          </li>
-          <li>
-            <Link to="/Home">Email</Link>
-          </li>
+class Nav extends Component {
+  render() {
+    const email = this.props.match.params.email;
+    return (
+      <div id="nav-container">
+        <nav className="nav">
+          <ul>
+            <li>
+              <Link to={`/home/${email}`}>Home</Link>
+            </li>
+            <li>
+              <Link to={`/home/${email}`}>Email</Link>
+            </li>
 
-          <li>
-            <Link to="/">Logout</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  );
+            <li>
+              <Link to="/">Logout</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
 }
 
 export default withRouter(Nav);
