2b34e405c9c7a8ed46c844647f653db55c0ee34c
MarjanGit <mansar16@huntersoe.org>
2019-12-10 14:37:56 -0500

    delete routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,14 @@
 import React, { Component } from "react";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
-import Home from "./components/home.js";
 import Home2 from "./components/Home2.js";
 import Splash from "./components/Splash.js";
-import EditLog from "./components/Edit.js";
-import AddLog from "./components/AddLog";
 import Login from "./components/Login";
 import Register from "./components/Register";
-import ViewData from "./components/ViewData";
 import Water from "./components/Water";
-import ViewDay from "./components/ViewDay";
 import Sleep from "./components/Sleep";
 import Calories from "./components/Calories";
 import Nav from "./components/Nav";
 
-import DeleteLog from "./components/DeleteLog";
 import { Provider, connect } from "react-redux";
 import store from "./store/index";
 import "./App.css";
@@ -27,15 +21,10 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={Login} />
             <Route exact path="/register" component={Register} />
-            <Route exact path="/home2:email" component={Home} />
             <Route exact path="/home/:email" component={Home2} />
-            <Route exact path="/edit/:email" component={EditLog} />
-            <Route exact path="/add/:email" component={AddLog} />
-            <Route exact path="/view/:email" component={ViewData} />
             <Route exact path="/water/:email" component={Water} />
             <Route exact path="/sleep/:email" component={Sleep} />
             <Route exact path="/calories/:email" component={Calories} />
-            <Route exact path="/delete/:email" component={DeleteLog} />
           </Switch>
         </Router>
       </Provider>
