5fcb079a6fc10ea21c42a3c2cd837fbbd9f1e9e2
MarjanGit <mansar16@huntersoe.org>
2019-12-10 12:23:48 -0500

    stuff in modal

--- client/src/components/Water.js
+++ client/src/components/Water.js
@@ -6,9 +6,9 @@ import "react-datepicker/dist/react-datepicker.css";
 import axios from "axios";
 import styles from "../Styling/Grid.css";
 import moment from "moment";
-import { Button, Modal } from 'react-bootstrap';
+import { Button, Modal } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import AddModal from "./AddModal.js"
+import AddModal from "./AddModal.js";
 
 class Water extends Component {
   constructor(props) {
@@ -24,150 +24,67 @@ class Water extends Component {
 
   componentDidMount() {
     this.setState({ startDate: new Date() }, () => {
-      console.log("Today is "+ this.state.startDate)
+      console.log("Today is " + this.state.startDate);
       this.getTotalForADate();
-    })
+    });
   }
 
-  // getTotalForAllDates(){
-  //   let url = "http://localhost:5000/api/water/groupbyday";
-  //   axios
-  //     .get(url, {
-  //       params: {
-  //         email: this.state.email
-  //       }
-  //     })
-  //     .then(res => {
-  //       this.setState({ waterLogs: res.data }, ()=>{
-  //         console.log(this.state.waterLogs)
-  //       });
-  //     })
-  //     .catch(error => {
-  //       console.log(error);
-  //     });
-  //     // this.getCurrentDayTotal();
-  //   // this.getAllDate()
-  // }
-
-  getTotalForADate=date=>{
-    console.log("You selected " + date)
-    // const{email, date}=req.query
-    const param=date?{email: this.state.email, date: date}:{email: this.state.email}
+  getTotalForADate = date => {
+    const param = date
+      ? { email: this.state.email, date: date }
+      : { email: this.state.email };
     let url = "http://localhost:5000/api/water/groupbyday";
     axios
       .get(url, {
         params: param
-        // {
-        //   email: this.state.email,
-        //   date: date
-        // }
       })
       .then(res => {
-        this.setState({ waterLogs: res.data }, ()=>{
-          console.log(this.state.waterLogs)
+        this.setState({ waterLogs: res.data }, () => {
+          console.log(this.state.waterLogs);
         });
       })
       .catch(error => {
         console.log(error);
       });
-      // this.getCurrentDayTotal();
-    // this.getAllDate()
-  }
+  };
 
-  // getCurrentDayTotal(){
-  //   console.log("Date" + new Date())
-  //   console.log(this.state.startDate)
-  //   let url = "http://localhost:5000/api/water/sumofaday";
-  //     axios
-  //       .get(url, {
-  //         params: {
-  //           email: this.state.email,
-  //           date: new Date()
-  //         }
-  //       })
-  //       .then(res => {
-  //         console.log(res.data);
-  //         console.log(res.data);
-  //         let records = res.data.map(element => {
-  //           this.setState({ totalDrankToday: element.total }, () => {
-  //             console.log("you drank "+ this.state.totalDrankToday + " oz today")
-  //           });
-  //         })
-          
-  //       })
-  //       .catch(error => {
-  //         console.log(error);
-  //       });
-  //     // this.getAllDate()
-  // }
-
-  // getAllDate() {
-  //   let url = "http://localhost:5000/api/water/all";
-  //   axios
-  //     .get(url, {
-  //       params: {
-  //         email: this.state.email
-  //       }
-  //     })
-  //     .then(res => {
-  //       this.setState({ waterLogs: res.data });
-  //     })
-  //     .catch(error => {
-  //       console.log(error);
-  //     });
-  // }
-
-  // filteredByDate(date) {
-  //   let url = "http://localhost:5000/api/water/bydate";
-  //   axios
-  //     .get(url, {
-  //       params: {
-  //         email: this.state.email,
-  //         date: moment(date).format("YYYY-MM-DD")
-  //       }
-  //     })
-  //     .then(res => {
-  //       this.setState({ waterLogs: res.data });
-  //     })
-  //     .catch(error => {
-  //       console.log(error);
-  //     });
-  // }
-
-  handleChange = date => {
-    this.setState({ startDate: date }, ()=> { 
+  handleDateChange = date => {
+    this.setState({ startDate: date }, () => {
       this.getTotalForADate(date);
-    })
+    });
+  };
 
-    // this.filteredByDate(date);
-  }
+  handleFormChange = date => {
+    this.setState({ startDate: date });
+  };
 
-  // addLog() {
-  //   console.log(this.amount);
-  //   console.log(this.state.startDate);
-  //   console.log(moment(this.state.startDate).format("YYYY-MM-DD"));
-  //   let url = "http://localhost:5000/api/water/add";
-  //   const data = {
-  //     amount: this.state.amount,
-  //     email: this.props.match.params.email,
-  //     date: moment(this.state.startDate).format("YYYY-MM-DD")
-  //   };
-  //   console.log(this.state.amount);
+  inputHandler = e => {
+    e.preventDefault();
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-  //   axios
-  //     .post(url, data)
-  //     .then(res => {
-  //       console.log(res);
-  //       console.log(res.data);
-  //       alert("Succesfully ADDED");
-  //     })
-  //     .catch(error => {
-  //       console.log(error);
-  //     });
-  // }
-  //need to add the :id to home url
+  addLog = e => {
+    e.preventDefault();
+    let url = "http://localhost:5000/api/water/add";
+    const data = {
+      amount: this.state.amount,
+      email: this.props.match.params.email,
+      date: moment(this.state.startDate).format("YYYY-MM-DD")
+    };
+    console.log(this.state.amount);
+
+    axios
+      .post(url, data)
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
   render() {
-    //console.log(this.state.startDate);
     let records = this.state.waterLogs.map(waterlog => {
       return (
         <tr key={waterlog.date}>
@@ -179,6 +96,43 @@ class Water extends Component {
         </tr>
       );
     });
+
+    let addForm = (
+      <div>
+        <DatePicker
+          selected={this.state.startDate}
+          onChange={this.handleFormChange}
+        />
+        <form
+          className="col-md-4 mb-3"
+          style={{
+            marginLeft: "auto",
+            marginRight: "auto",
+            marginTop: "2%"
+          }}
+          onSubmit={this.addLog}
+        >
+          <div className="form-group">
+            <label style={{ fontWeight: "bold" }}>Amount (oz): </label>
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              name={"amount"}
+              pattern="[0-9]*"
+              onChange={this.inputHandler}
+            />
+          </div>
+
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Add to Water Log"
+              className="btn btn-primary"
+            />
+          </div>
+        </form>
+      </div>
+    );
 
     return (
       <div>
@@ -202,18 +156,17 @@ class Water extends Component {
                 padding: "50px"
               }}
             >
-              <h1 style={{ color: "#47a02c" }}> You have drank: {this.state.totalDrankToday} oz today</h1>
-              <AddModal/>
-              {/* <button className="addbutton" onClick={() => this.addLog}>
-                Add Log
-              </button> */}
+              <h1 style={{ color: "#47a02c" }}>
+                You have drank: {this.state.totalDrankToday} oz today
+              </h1>
+              <AddModal form={addForm} />
             </div>
 
             <div style={{ padding: "50px" }}>
               Choose Date:
               <DatePicker
                 selected={this.state.startDate}
-                onChange={this.handleChange}
+                onChange={this.handleDateChange}
               />
               <h3
                 className="text-dark"
@@ -225,8 +178,9 @@ class Water extends Component {
               >
                 {this.state.startDate.toDateString()}
               </h3>
+              <button onClick={() => this.getTotalForADate()}>View All</button>
             </div>
-            <button onClick={()=>this.getTotalForADate()}>View All</button>
+
             <div>
               <table
                 className="datatable"
