90fc59d2c94848284c133ce05b98e24bd6a78549
yitongni <31358391+yitongni@users.noreply.github.com>
2019-12-12 14:08:38 -0500

    When registering it makes sure user does not exist

--- api/User.js
+++ api/User.js
@@ -37,6 +37,27 @@ router.get("/login", async (req, res, next) => {
     });
 });
 
+router.get("/find", async (req, res, next) => {
+  // let found = Users.findAndCountAll({
+  //   where: {
+  //     email: req.query.email
+  //   }
+  // });
+  console.log(req.query.email)
+  Users.findAndCountAll({
+    where: {
+      email: req.query.email
+    }
+  })
+    .then(userResponse => {
+      console.log(userResponse);
+      res.status(200).json(userResponse);
+    })
+    .catch(error => {
+      res.status(400).send(error);
+    });
+});
+
 // router.post("/create", async (req, res, next) => {
 //   const { password, email } = req.body;
 //   console.log(req.body);
@@ -71,21 +92,37 @@ router.post("/create", async (req, res, next) => {
   const { username, password, email } = req.body;
   console.log(req.body);
 
-  Users.findOrCreate({
-    where: {
-      email,
-      password
-    }
-  }).then(result => {
-    let user = result[0]; // the instance of the user
-    let created = result[1]; // boolean stating if it was created or not
+  try {
+    const created = await Users.create({
+      password,
+      email
+    });
+    console.log(`created ${created.email}!`);
+    res.status(201).send({
+      password,
+      email
+    });
+  } 
+  catch (err) {
+    res.status(400).send(error);
+    console.error(err);
+  }
+        
+  // Users.findOrCreate({
+  //   where: {
+  //     email,
+  //     password
+  //   }
+  // }).then(result => {
+  //   let user = result[0]; // the instance of the user
+  //   let created = result[1]; // boolean stating if it was created or not
 
-    if (!created) {
-      res.status(400).send(result);
-      console.log("user already exists");
-    } else {
-      res.status(200).send(result);
-      console.log("Created user...");
-    }
-  });
+  //   if (!created) {
+  //     res.status(400).send(result);
+  //     console.log("user already exists");
+  //   } else {
+  //     res.status(200).send(result);
+  //     console.log("Created user...");
+  //   }
+  // });
 });
